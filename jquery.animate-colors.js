8bf930cd506714fd8b3c21a7731ede1e410da57e
Edwin Martin <edwin@bitstorm.org>
2013-10-31 22:36:58 +0100

    Reformatted list of colors for Google Closure Compiler, increased version

--- jquery.animate-colors.js
+++ jquery.animate-colors.js
@@ -1,7 +1,7 @@
 /**!
- * @preserve Color animation 20120928
+ * @preserve Color animation 20131031
  * http://www.bitstorm.org/jquery/color-animation/
- * Copyright 2011, 2012 Edwin Martin <edwin@bitstorm.org>
+ * Copyright 2011, 2013 Edwin Martin <edwin@bitstorm.org>
  * Released under the MIT and GPL licenses.
  */
 
@@ -106,57 +106,56 @@
 		} else {
 			quadruplet = colors[color];
 		}
-
 		return quadruplet;
 	}
 
-	// Some named colors to work with
+	// Some named colors to work with, added by Bradley Ayers
 	// From Interface by Stefan Petre
 	// http://interface.eyecon.ro/
 	var colors = {
-		aqua:[0,255,255,1],
-		azure:[240,255,255,1],
-		beige:[245,245,220,1],
-		black:[0,0,0,1],
-		blue:[0,0,255,1],
-		brown:[165,42,42,1],
-		cyan:[0,255,255,1],
-		darkblue:[0,0,139,1],
-		darkcyan:[0,139,139,1],
-		darkgrey:[169,169,169,1],
-		darkgreen:[0,100,0,1],
-		darkkhaki:[189,183,107,1],
-		darkmagenta:[139,0,139,1],
-		darkolivegreen:[85,107,47,1],
-		darkorange:[255,140,0,1],
-		darkorchid:[153,50,204,1],
-		darkred:[139,0,0,1],
-		darksalmon:[233,150,122,1],
-		darkviolet:[148,0,211,1],
-		fuchsia:[255,0,255,1],
-		gold:[255,215,0,1],
-		green:[0,128,0,1],
-		indigo:[75,0,130,1],
-		khaki:[240,230,140,1],
-		lightblue:[173,216,230,1],
-		lightcyan:[224,255,255,1],
-		lightgreen:[144,238,144,1],
-		lightgrey:[211,211,211,1],
-		lightpink:[255,182,193,1],
-		lightyellow:[255,255,224,1],
-		lime:[0,255,0,1],
-		magenta:[255,0,255,1],
-		maroon:[128,0,0,1],
-		navy:[0,0,128,1],
-		olive:[128,128,0,1],
-		orange:[255,165,0,1],
-		pink:[255,192,203,1],
-		purple:[128,0,128,1],
-		violet:[128,0,128,1],
-		red:[255,0,0,1],
-		silver:[192,192,192,1],
-		white:[255,255,255,1],
-		yellow:[255,255,0,1],
-		transparent:[255,255,255,0]
+		'aqua': [0,255,255,1],
+		'azure': [240,255,255,1],
+		'beige': [245,245,220,1],
+		'black': [0,0,0,1],
+		'blue': [0,0,255,1],
+		'brown': [165,42,42,1],
+		'cyan': [0,255,255,1],
+		'darkblue': [0,0,139,1],
+		'darkcyan': [0,139,139,1],
+		'darkgrey': [169,169,169,1],
+		'darkgreen': [0,100,0,1],
+		'darkkhaki': [189,183,107,1],
+		'darkmagenta': [139,0,139,1],
+		'darkolivegreen': [85,107,47,1],
+		'darkorange': [255,140,0,1],
+		'darkorchid': [153,50,204,1],
+		'darkred': [139,0,0,1],
+		'darksalmon': [233,150,122,1],
+		'darkviolet': [148,0,211,1],
+		'fuchsia': [255,0,255,1],
+		'gold': [255,215,0,1],
+		'green': [0,128,0,1],
+		'indigo': [75,0,130,1],
+		'khaki': [240,230,140,1],
+		'lightblue': [173,216,230,1],
+		'lightcyan': [224,255,255,1],
+		'lightgreen': [144,238,144,1],
+		'lightgrey': [211,211,211,1],
+		'lightpink': [255,182,193,1],
+		'lightyellow': [255,255,224,1],
+		'lime': [0,255,0,1],
+		'magenta': [255,0,255,1],
+		'maroon': [128,0,0,1],
+		'navy': [0,0,128,1],
+		'olive': [128,128,0,1],
+		'orange': [255,165,0,1],
+		'pink': [255,192,203,1],
+		'purple': [128,0,128,1],
+		'violet': [128,0,128,1],
+		'red': [255,0,0,1],
+		'silver': [192,192,192,1],
+		'white': [255,255,255,1],
+		'yellow': [255,255,0,1],
+		'transparent': [255,255,255,0]
 	};
 })(jQuery);
